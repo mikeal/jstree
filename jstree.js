1692c9bac497313bd7612c54242f065b42c2f7d6
Mikeal Rogers <mikeal.rogers@gmail.com>
2010-04-05 16:55:08 -0700

    Moved insertion logic to node, cleaned up.

--- jstree.js
+++ jstree.js
@@ -28,9 +28,54 @@
 var createNode = function (parent, node) {
   var node = node || {};
   node.values = node.values || [];
-  
-  
-  
+  node.size = 1;
+  node.insertNode = function (n) {
+    if (n.key === node.key) {
+      throw "Node with identical key cannot be inserted."
+    }
+    if (n.key < node.key) {
+      if (!node.left) {
+        node.left = n;
+        n.reParent(node);
+      } else {
+        if (node.left.key === n.key) {
+          throw "Node with identical key cannot be inserted."
+        }
+        if (node.left.key > n.key) {
+          node.left.insertNode(n);
+        } else {
+          n.insertNode(node.left);
+          node.left = n;
+          n.reParent(node)
+          node.left;
+        }
+      }
+    } else {
+      if (!node.right) {
+        node.right = n;
+        n.reParent(node);
+      } else {
+        if (node.right.key === n.key) {
+          throw "Node with identical key cannot be inserted."
+        }
+        if (node.right.key < n.key) {
+          node.left.insertNode(n);
+        } else {
+          n.insertNode(node.right);
+          node.right = n;
+          n.reParent(node);
+          node.right;
+        }
+      }
+    }
+  }
+  node.reParent = function (p) {
+    node.parent = function (){return p};
+    while (p) {
+      p.size = p.right + p.left + 1;
+      p = p.parent();
+    }
+  }
   node.parent = function (){return parent;};
   node.range = function (end) {
     var results = [];
@@ -57,37 +102,53 @@ var createTree(tree) {
       tree.root = createNode(null, {key:key,values:[value]});
       return tree.root;
     }
-    var n = tree.root;
-    while (n.key !== key) {
-      if (key < n.key) {
-        if (n.left) {
-          if (n.left > key) {
-            var l = n.left;
-            node = createNode(n, {key:key,right:l});
-            node.right.parent = function() {return node;};
-            n.left = node;
-          } 
-          n = n.left;
-        } else {
-          n = createNode(n, {key:key});
-        }
-        n = node.left;
+    var traverse = function (key, n) {
+      if (n.key === key) {return n;};
+      if (n.left > key) {
+        return n;
       } else {
-        if (n.right) {
-          if (n.right < key){
-            var r = n.right;
-            node = createNode(n, {key:key,left:r});
-            node.left.parent = function () {return node;};
-            n.right = node;
-          }
-          n = node.right;
-        } else {
-          n = createNode(n, {key:key});
-        }
-      } 
+        return traverse(key, n)
+      }
     }
-    n.values.push(value);
-    return n
+    var node = traverse(key, tree.root)
+    if (node.key === key) {
+      node.values.push(value);
+      return node;
+    }
+    var n = createNode(node, {key:key,values:[value]});
+    node.insertNode(n);
+    return n;
+    
+    // while (n.key !== key) {
+    //   if (key < n.key) {
+    //     if (n.left) {
+    //       if (n.left > key) {
+    //         var l = n.left;
+    //         node = createNode(n, {key:key,right:l});
+    //         node.right.parent = function() {return node;};
+    //         n.left = node;
+    //       } 
+    //       n = n.left;
+    //     } else {
+    //       n = createNode(n, {key:key});
+    //     }
+    //     n = node.left;
+    //   } else {
+    //     if (n.right) {
+    //       if (n.right < key){
+    //         var r = n.right;
+    //         node = createNode(n, {key:key,left:r});
+    //         node.left.parent = function () {return node;};
+    //         n.right = node;
+    //       }
+    //       n = node.right;
+    //     } else {
+    //       n = createNode(n, {key:key});
+    //     }
+    //   } 
+    // }
+    // n.values.push(value);
+    // return n
   }
   tree.find = tree.find || function (key) {
     if (!tree.root)  {return null;}
@@ -130,14 +191,7 @@ var createTree(tree) {
   }
   tree.range = tree.range || function (start, end) {
     if (!tree.root) {return []};
-    var traverse = function (key, n) {
-      if (n.key === key) {return n;};
-      if (n.left > key) {
-        return n;
-      } else {
-        return traverse(key, n)
-      }
-    }
+    
     return traverse(start, tree.root).range(end);
   }
 }
